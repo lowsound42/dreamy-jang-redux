9de337041a1cc133612b1e4f674fa75d9f14aa69
omario <o.khandxb@gmail.com>
2020-07-02 17:42:55 -0400

    csv download added

--- src/Report.js
+++ src/Report.js
@@ -30,10 +30,11 @@ function reducer(state, action) {
 function Report() {
 
    //redux initialization (setting state + setting dispatch variable)
-   const shelterArray = useSelector(state => state);
-   const dispatch = useDispatch();
+  const shelterArray = useSelector(state => state);
+  const dispatch = useDispatch();
 
-   let tempArray = [["Run a report and THEN click here! Some day this will be a proper website, I promise"]];
+  let tempArray = [["Run a report and THEN click here! Some day this will be a proper website, I promise"]];
+  let beginDay, endDay;
 
   const shelterURL = 'https://blooming-castle-18936.herokuapp.com/shelterData';
   const url = 'http://localhost:8080/shelterData'
@@ -44,14 +45,14 @@ function Report() {
   const [sectorTest, setSectorTest] = useState(false);
   const [shelterTest, setShelterTest] = useState(false);
   const [buttonTest, setButtonTest] = useState(false);
-  const [downloadData, setDownloadData] = useState([['run'],['a'],['query'],['before'],['clikcing'],['here!']]);
+  const [csvButton, setCsvButton] = useState(false);
 
   const sectorOptions = [
     'Pick a sector here and a date above', 'Women', 'Men', 'Families', 'Co-ed', 'Youth'
   ];
   const sectorDefaultOption = sectorOptions[0];
   const shelterOptions = [
-    'Pick a shelter here and a date above', "COSTI - Radisson Hotel (Single)","COSTI - Toronto Plaza Hotel(Fam)","COSTI - Toronto Plaza Hotel(Fam)","COSTI Radisson Hotel","COSTI Radisson Hotel","COSTI Reception Centre","COSTI Toronto Plaza Hotel (Sin)","Christie Ossington Men's Hostel","Christie Ossington South Hostel","Christie Refugee Welcome Centre","Birkdale Residence","Birkdale Residence Motel (Fam)","Birkdale Residence Motel (Men's)","Downsview Dells 1651 Sheppard W","Family Residence - Edward  Co-Ed","Family Residence - Edward Villag","Family Residence - LI Site","Family Residence - MA Site","Family Residence - Main Building","Family Residence - New Plaza","Family Residence - Roycroft Site","Fort York - Staybridge Suites","Fort York Res 38 Bathurst St.","Fort York Res 38 Bathurst St.","Fort York Res 38 Bathurst St.","Robertson House","3306 Kingston Road","Junction Place","Seaton House - 339 George Street","Seaton House - 339 George Street","Seaton House - 339 George Street","Seaton House - 339 George Street","Seaton House - 339 George Street","Seaton House - 339 George Street","Seaton House - 339 George Street","129 Peter St","Womens Res-Alexandra Hotel","Womens' Res - 63 Bellwoods Ave.","Womens' Res - 674 Dundas St. W","Womens' Res - 674 Dundas St. W","Cornerstone 616 Vaughan Rd.","Covenant House-20 Gerrard St. E.","Covenant House-20 Gerrard St. E.","Covenant House-21 McGill Street","Dixon Hall - Heyworth House","Dixon Hall - Heyworth House","Dixon Hall-School House","Eva's Phoenix","Eva's Place","Eva's Satellite","Eva's Satellite","Fife House - Denison Program","Fife-Sherbourne Transitional Pgm","Fred Victor 512 Jarvis St.","Fred Victor T2H 386 Dundas St. E","FV Women's Hostel 86 Lombard","FV Women's Hostel 86 Lombard","Fred Victor BUS - 1161 Caledonia","Fred Victor BUS - 1161 Caledonia","Barrett House","Good Shepherd Centre","Good Shepherd Centre","HFS - 702 Kennedy Shelter","HFS - Savard's Womens' Shelter","HFS - 545 Lakeshore Blvd W.Women","HFS - 545 Lakeshore Blvd. W. Men","HFS - 545 Lakeshore Blvd. W. Men","HFS - Refugee Response Centre -","HFS - Refugee Response Centre -","HFS - Scarborough Shelter","HFS-545 Lakeshore Blvd W.Couple","HFS - Strachan House","HFS - Strachan House","Horizons for Youth","Kennedy House Youth Shelter","Na-Me-Res 14 Vaughan Road","Sagatay 26 Vaughan Road","Eagle's Nest Transition House","SVDP - Amelie House","SVDP - Elisa House","SVDP - Mary's Home","SVDP - Mary's Home","SVDP - St. Clare's Residence","St.Simon's 525 Bloor Street East","St.Simon's 525 Bloor Street East","Street Haven - 87 Pembroke St","Street Haven - 87 Pembroke St","Sojourn House","Sojourn House(Singles)","Sojourn House-Grange (Family)","S.A. Evangeline Residence","S.A. Evangeline Residence","Florence Booth - 723 Queen St. W","Florence Booth - 723 Queen St. W","S.A. Gateway - 107 Jarvis Street","S.A. Gateway - 107 Jarvis Street","S.A. Maxwell Meighen Centre","S.A. Maxwell Meighen Centre","S.A. Maxwell Meighen Centre","S.A. Maxwell Meighen Centre","S.A. New Hope Leslieville","2671 Islington Avenue-Sr.Female","2671 Islington Avenue-Sr.Male","Scott Mission - 502 Spadina Ave","Scott Mission - 502 Spadina Ave","TCH 191 Spadina Rd(Singles)","Tor. Comm. Hostel 191 Spadina Rd","Turning Point Youth Services","Red Door - 1430 Gerrad St E","Yhouse - Vanauley","YMCA Sprott House Walmer Rd","YWCA-80 Woodlawn Av. E.-Women","YWCA-80 Woodlawn Ave. E.-Youth","YWCA - 348 Davenport Road Women","YWCA - 348 Davenport Road Youth","Youth w/o Shelter Emerg Shelter","Youth w/o Shltr Transitional Res","YouthLink - 747 Warden Ave","YouthLink - 747 Warden Ave"
+    'Pick a shelter here and a date above', "COSTI - Radisson Hotel (Single)","COSTI - Toronto Plaza Hotel(Fam)","COSTI - Toronto Plaza Hotel(Fam)","COSTI Radisson Hotel","COSTI Radisson Hotel","COSTI Reception Centre","COSTI Toronto Plaza Hotel (Sin)","Christie Ossington Men's Hostel","Christie Ossington South Hostel","Christie Refugee Welcome Centre","Birkdale Residence","Birkdale Residence Motel (Fam)","Birkdale Residence Motel (Men's)","Downsview Dells 1651 Sheppard W","Family Residence - Edward  Co-Ed","Family Residence - Edward Villag","Family Residence - LI Site","Family Residence - MA Site","Family Residence - Main Building","Family Residence - New Plaza","Family Residence - Roycroft Site","Fort York - Staybridge Suites","Fort York Res 38 Bathurst St.","Fort York Res 38 Bathurst St.","Fort York Res 38 Bathurst St.","Robertson House","3306 Kingston Road","Junction Place","Seaton House - 339 George Street","Seaton House - 339 George Street","Seaton House - 339 George Street","Seaton House - 339 George Street","Seaton House - 339 George Street","Seaton House - 339 George Street","Seaton House - 339 George Street","129 Peter St","Womens Res-Alexandra Hotel","Womens' Res - 63 Bellwoods Ave.","Womens' Res - 674 Dundas St. W","Womens' Res - 674 Dundas St. W","Cornerstone 616 Vaughan Rd.","Covenant House-20 Gerrard St. E.","Covenant House-20 Gerrard St. E.","Covenant House-21 McGill Street","Dixon Hall - Heyworth House","Dixon Hall-School House","Eva's Phoenix","Eva's Place","Eva's Satellite","Eva's Satellite","Fife House - Denison Program","Fife-Sherbourne Transitional Pgm","Fred Victor 512 Jarvis St.","Fred Victor T2H 386 Dundas St. E","FV Women's Hostel 86 Lombard","FV Women's Hostel 86 Lombard","Fred Victor BUS - 1161 Caledonia","Fred Victor BUS - 1161 Caledonia","Barrett House","Good Shepherd Centre","Good Shepherd Centre","HFS - 702 Kennedy Shelter","HFS - Savard's Womens' Shelter","HFS - 545 Lakeshore Blvd W.Women","HFS - 545 Lakeshore Blvd. W. Men","HFS - 545 Lakeshore Blvd. W. Men","HFS - Refugee Response Centre -","HFS - Refugee Response Centre -","HFS - Scarborough Shelter","HFS-545 Lakeshore Blvd W.Couple","HFS - Strachan House","HFS - Strachan House","Horizons for Youth","Kennedy House Youth Shelter","Na-Me-Res 14 Vaughan Road","Sagatay 26 Vaughan Road","Eagle's Nest Transition House","SVDP - Amelie House","SVDP - Elisa House","SVDP - Mary's Home","SVDP - Mary's Home","SVDP - St. Clare's Residence","St.Simon's 525 Bloor Street East","St.Simon's 525 Bloor Street East","Street Haven - 87 Pembroke St","Street Haven - 87 Pembroke St","Sojourn House","Sojourn House(Singles)","Sojourn House-Grange (Family)","S.A. Evangeline Residence","S.A. Evangeline Residence","Florence Booth - 723 Queen St. W","Florence Booth - 723 Queen St. W","S.A. Gateway - 107 Jarvis Street","S.A. Gateway - 107 Jarvis Street","S.A. Maxwell Meighen Centre","S.A. Maxwell Meighen Centre","S.A. Maxwell Meighen Centre","S.A. Maxwell Meighen Centre","S.A. New Hope Leslieville","2671 Islington Avenue-Sr.Female","2671 Islington Avenue-Sr.Male","Scott Mission - 502 Spadina Ave","Scott Mission - 502 Spadina Ave","TCH 191 Spadina Rd(Singles)","Tor. Comm. Hostel 191 Spadina Rd","Turning Point Youth Services","Red Door - 1430 Gerrad St E","Yhouse - Vanauley","YMCA Sprott House Walmer Rd","YWCA-80 Woodlawn Av. E.-Women","YWCA-80 Woodlawn Ave. E.-Youth","YWCA - 348 Davenport Road Women","YWCA - 348 Davenport Road Youth","Youth w/o Shelter Emerg Shelter","Youth w/o Shltr Transitional Res","YouthLink - 747 Warden Ave","YouthLink - 747 Warden Ave"
   ]
   const shelterDefaultOption = shelterOptions[0];
 
@@ -107,11 +108,14 @@ function Report() {
  
 
   const sendData = () => {
+    setCsvButton(true);
     console.log(state);
     async function setPackage() {
       var dateArray = [];
       var currentDate = moment(state.startDate);
       var stopDate = moment(state.endDate);
+      beginDay = currentDate;
+      endDay = stopDate;
       while (currentDate <= stopDate) {
           dateArray.push( moment(currentDate).format('YYYY-MM-DD') )
           currentDate = moment(currentDate).add(1, 'days');
@@ -175,12 +179,6 @@ function Report() {
           fontFamily: "system-ui, -apple-system",
           datepickerPosition: 'left',
           datepickerBorderRadius: '10px',
-          colors: {
-            accessibility: "#D80249",
-            selectedDay: "#f7518b",
-            selectedDayHover: "#F75D95",
-            primaryColor: "#d8366f"
-          }
         }
       }}
     >
@@ -219,14 +217,14 @@ function Report() {
                </tbody>
             </table>
              : null}
-            <CSVLink
+            {csvButton ? <CSVLink
             data={tempArray}
-            filename={"my-file.csv"}
-            className="btn btn-primary"
+            filename={`shelterData-${sector ? sector : selectedShelter}.csv`}
+            className="csvButton"
             target="_blank"
           >
             Download me
-          </CSVLink>;
+          </CSVLink> : <p>Scroll all the way down to download an excel file when you generate a table</p>}
     </div>
   )
 }
